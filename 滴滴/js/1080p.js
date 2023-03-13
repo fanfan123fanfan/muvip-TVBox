5b0345f2d42e39e41f0fe7d1f129728724eb8d04
mullvip <127660741+mullvip@users.noreply.github.com>
2023-03-13 13:57:59 +0800

    Create 1080p.js

---
+++ 滴滴/js/1080p.js
@@ -0,0 +1,7 @@
+变种 规则 = 对象。分配（muban.mxpro ，{ _ _
+标题：'1080P' ，
+主持人：'https://1080p.tv' ，
+url : '/vodshow/fyclass--------fypage---/' ,
+searchUrl : '/vodsearch/**------------fypage---/' ,
+class_parse : '.navbar-items li:gt(1):lt(7);a&&Text;a&&href;.*/(.*?)/' ,
+} ) ;
